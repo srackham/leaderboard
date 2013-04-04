f5fd95d46249c72a4919fb6a07884e90788236e0
Stuart Rackham <srackham@gmail.com>
2013-04-04 17:44:04 +1300

    Moved server-only model functions from common to server.

--- common/model.ts
+++ common/model.ts
@@ -7,39 +7,4 @@ module Model {
 
   export var Players = new Meteor.Collection('players');
 
-  Players.allow({
-    // The user must be logged in to change the data.
-    insert: userId => userId,
-    update: userId => userId,
-    remove: userId => userId
-  });
-
-  // Delete all existing documents then insert new ones.
-  // Synchronous, server only.
-  // Throws exception if on error.
-  Players.reset_data = function () {
-    var names = [
-      'Ada Lovelace',
-      'Grace Hopper',
-      'Marie Curie',
-      'Carl Friedrich Gauss',
-      'Nikola Tesla',
-      'Claude Shannon',
-      'Issac Newton'
-    ];
-    if (!Meteor.isServer) {
-      throw new Meteor.Error('Server only');
-    }
-    console.log('Resetting data.');
-    Players.remove({});
-    for (var i in names) {
-      Players.insert(
-          {
-            name: names[i],
-            score: Math.floor(Math.random() * 10) * 5
-          }
-      );
-    }
-  }
-
 }
--- server/main.ts
+++ server/main.ts
@@ -12,6 +12,41 @@ module Server {
     return Players.find();
   });
 
+  Players.allow({
+    // The user must be logged in to change the data.
+    insert: userId => userId,
+    update: userId => userId,
+    remove: userId => userId
+  });
+
+  // Delete all existing documents then insert new ones.
+  // Synchronous, server only.
+  // Throws exception if on error.
+  Players.reset_data = function () {
+    var names = [
+      'Ada Lovelace',
+      'Grace Hopper',
+      'Marie Curie',
+      'Carl Friedrich Gauss',
+      'Nikola Tesla',
+      'Claude Shannon',
+      'Issac Newton'
+    ];
+    if (!Meteor.isServer) {
+      throw new Meteor.Error('Server only');
+    }
+    console.log('Resetting data.');
+    this.remove({});
+    for (var i in names) {
+      this.insert(
+          {
+            name: names[i],
+            score: Math.floor(Math.random() * 10) * 5
+          }
+      );
+    }
+  }
+
   Meteor.startup(function() {
     // Create some players if the database is empty.
     if (Players.find().count() === 0) {
